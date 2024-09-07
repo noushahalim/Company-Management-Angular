e4cb0631a355dd9e34227973b3341a933973b9b2
noushahalim <noushahalim@gmail.com>
2024-09-07 14:55:49 +0530

    login page UI completed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'office':"url('/assets/images/office.png')"
+      }
+    },
   },
   plugins: [],
 }
